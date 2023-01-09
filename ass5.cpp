1fdf1e4c79aef8c652dbbe687a95270c923948b8
Mohamed Sabry <99232657+moSabryii@users.noreply.github.com>
2023-01-09 20:04:12 +0200

    Create ass5.cpp

---
+++ ass5.cpp
@@ -0,0 +1,13 @@
+// This Is My First File
+// In Learning Programming
+// Iam Happy <3
+#include <iostream> /* Preprocessor Directive */
+
+int main()
+{
+    std::cout << "Line One\n";
+    std::cout << "Line Two\n"; // cout Function
+    std::cout << "Line Three\n";
+    // Return 0
+    return 0;
+} // End Main Function
