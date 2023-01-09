8928d18cc71fd07cee6a49abd66dc8efb9a7a784
Mohamed Sabry <99232657+moSabryii@users.noreply.github.com>
2023-01-09 20:00:58 +0200

    Create ass4.cpp

---
+++ ass4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"|=======================|"<<endl;
+    cout<<"|== Elzero web school ==|"<<endl;
+    cout<<"|==        C++        ==|"<<endl;
+    cout<<"|=======================|";
+
+    return 0;
+}
